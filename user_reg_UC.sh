5d4e3558711210fda8f0893f28cf5a61c0379438
codexakshay <codexakshay@gmail.com>
2020-09-08 14:08:41 +0530

    user_reg_UC.sh

--- user_reg_UC.sh
+++ user_reg_UC.sh
@@ -1,28 +1,47 @@
-echo -e "ENTER FIRST NAME : \c"echo -e "ENTER MOBILE NUMBER : +91 \c"
-read mn
-mn_val="^[0-9]{10}$"
+#REGEX
 
-read f_n
 f_n_val="^[Cap]+[a-z]*$"
 
-echo -e "ENTER LAST NAME : \c"
-read l_n
 l_n_val="^[Cap]+[a-z]*$"
 
-
-echo -e "ENTER EMAIL : \c"
-read e
 e_val="^[abc]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$"
 
-
-echo -e "ENTER MOBILE NUMBER : +91 \c"
-read mn
 mn_val="^[0-9]{10}$"
 
-echo -e "ENTER PASSWORD : \c"
-read pwd
 pwd_val="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$"
 
+echo -e "\n    --REGEX & VALIDATION--\n"
+echo -e "\n1. USER PROFILE REGISTRATION"
+echo -e "\n2. SEPERATE EMAIL VALIDATION"
+
+echo -e "\nENTER CHOICE : \c"
+read ch
+
+if [ $ch -eq 1 ]
+then
+#USER PROFILE REGISTERATION
+
+#READING PROFILE DATA 
+
+	echo -e "\nENTER FIRST NAME : \c"
+	read f_n
+
+	echo -e "\nENTER LAST NAME : \c"
+        read l_n
+
+	echo -e "\nENTER EMAIL : \c"
+        read e
+
+	echo -e "\nENTER MOBILE NUMBER : +91 \c"
+        read mn
+
+	echo -e "\nENTER PASSWORD : \c"
+        read pwd
+
+
+#USER DATA VALIDATION
+
+	echo -e "\n\n   --ENTERED DATA--\n"
 
 #FIRST NAME
 
@@ -35,39 +54,151 @@ pwd_val="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$"
 
 #LAST NAME
 
+
         if ! [[ $l_n =~ $l_n_val ]]
         then
-                echo "INVALID LAST NAME"
+                echo -e "\nINVALID LAST NAME"
         else
-                echo "LAST NAME : $l_n"
+                echo -e "\nLAST NAME : $l_n"
         fi
 
 
 #EMAIL
 
+
         if ! [[ $e =~ $e_val ]]
         then
-                echo "INVALID EMAIL"
+                echo -e "\nINVALID EMAIL"
         else
-                echo "USER EMAIL : $e"
+                echo -e "\nUSER EMAIL : $e"
         fi
 
 
 #MOBILE NUMBER
 
+
         if ! [[ $mn =~ $mn_val ]]
         then
-                echo "INVALID CONTACT NUMBER"
+                echo -e "\nINVALID CONTACT NUMBER"
         else
-                echo "USER CONTACT NUMBER : +91 $mn"
+                echo -e "\nUSER CONTACT NUMBER : +91 $mn"
         fi
 
 
 #PASSWORD
 
+
         if ! [[ $pwd =~ $pwd_val ]]
         then
-                echo "INVALID PASSWORD"
+                echo -e "\nINVALID PASSWORD"
         else
-                echo "USER PASSWORD IS SAFE WITH US"
+                echo -e "\nUSER PASSWORD IS SAFE WITH US"
         fi
+
+else
+
+#SEPERATE CHECK
+
+echo -e "\n\tEMAIL VALIDATION LIST\n"
+	
+	echo -e	"\n  • 1. abc@yahoo.com"
+	echo -e	"\n  • 2. abc-100@yahoo.com"
+	echo -e	"\n  • 3. abc.100@yahoo.com"
+	echo -e	"\n  • 4. abc111@abc.com"
+	echo -e	"\n  • 5. abc-100@abc.net"
+	echo -e	"\n  • 6. abc.100@abc.com.au"
+	echo -e	"\n  • 7. abc@1.com"
+	echo -e	"\n  • 8. abc@gmail.com.com"
+	echo -e	"\n  • 9. abc+100@gmail.com"
+		
+	echo -e "\nENTER YOUR CHOICE : \c"
+	read ch1
+		case $ch1 in
+				1)
+					e="abc@yahoo.com"
+					if ! [[ $e =~ $e_val ]]
+       					then
+                				echo -e "\nINVALID EMAIL : $e"
+        				else
+                				echo -e "\nVALID EMAIL : $e"
+        				fi
+					;;
+				2)
+  					e="abc-100@yahoo.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+
+				3)
+					e="abc.100@yahoo.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				4)
+					e="abc111@abc.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				5)
+					e="abc-100@abc.net"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				6)
+					e="abc.100@abc.com.au"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				7)
+					e="abc@1.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				8)
+					e="abc@gmail.com.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+				9)
+					e="abc+100@gmail.com"
+                                        if ! [[ $e =~ $e_val ]]
+                                        then
+                                                echo -e "\nINVALID EMAIL : $e"
+                                        else
+                                                echo -e "\nVALID EMAIL : $e"
+                                        fi
+                                        ;;
+
+				*)
+					echo -e "\nENTER VALID CHOICE (1-9)"
+					;;
+			esac
+fi
