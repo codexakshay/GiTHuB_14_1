a2d3867a229c01090055c0453fc6deff0e2f5172
codexakshay <codexakshay@gmail.com>
2020-09-08 12:11:57 +0530

    user_reg_UC

--- user_reg_UC.sh
+++ user_reg_UC.sh
@@ -2,9 +2,26 @@ echo -e "ENTER FIRST NAME : \c"
 read f_n
 f_n_val="^[Cap]+[a-z]*$"
 
+echo -e "ENTER LAST NAME : \c"
+read l_n
+l_n_val="^[Cap]+[a-z]*$"
+
+
+#FIRST NAME
+
 	if ! [[ $f_n =~ $f_n_val ]]
 	then
 		echo "INVALID INPUT"
 	else
 		echo "FIRST NAME : $f_n"
 	fi
+
+
+#LAST NAME
+
+        if ! [[ $l_n =~ $l_n_val ]]
+        then
+                echo "INVALID INPUT"
+        else
+                echo "LAST NAME : $l_n"
+        fi
